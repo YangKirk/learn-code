4e9bec6753b5211b2d411bd248756cca42fe377f
Kirk <Yang.Kirk@inventec.com.cn>
2021-01-24 05:47:26 +0800

    [修改]cook_book_1问题3

--- level_8/cook_book_1.py
+++ level_8/cook_book_1.py
@@ -77,3 +77,25 @@ record = ('ACME', 50, 123.45, (12, 18, 2012))
 name, *_, (*_, year) = record
 print(name, year)
 """
+
+# 问题3：在迭代操作或者其他操作的时候，怎样只保留最后有限几个元素的历史记录？
+
+from collections import deque
+
+
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+
+
+# Example use on a file
+if __name__ == '__main__':
+    with open(r'../../cookbook/somefile.txt') as f:
+        for line, prevlines in search(f, 'python', 5):
+            for pline in prevlines:
+                print(pline, end='')
+            print(line, end='')
+            print('-' * 20)
